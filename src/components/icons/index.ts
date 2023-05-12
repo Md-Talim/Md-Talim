d4beed607ee30a00566063927adbde06c62bcb38
Mohammad Talim <talimmohammad116@gmail.com>
2023-05-12 20:41:49 +0530

    Add a Home Icon

--- src/components/icons/index.ts
+++ src/components/icons/index.ts
@@ -1,6 +1,7 @@
 import ArrowIcon from './ArrowIcon';
 import GithubIcon from './GithubIcon';
 import GmailIcon from './GmailIcon';
+import HomeIcon from './HomeIcon';
 import LinkedinIcon from './LinkedinIcon';
 
-export { ArrowIcon, GithubIcon, GmailIcon, LinkedinIcon };
+export { ArrowIcon, GithubIcon, GmailIcon, HomeIcon, LinkedinIcon };
