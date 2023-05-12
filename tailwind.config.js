78eccb92ddd0312fe9cfa37171836819bcbf14b2
Mohammad Talim <talimmohammad116@gmail.com>
2023-05-12 14:59:40 +0530

    Customize the default theme

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,18 @@
 export default {
   content: ['./src/**/*.{js,jsx,ts,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        dark: '#7D8F69',
+        darker: '#557153',
+        light: '#A9AF7E',
+        lighter: '#E6E5A3',
+      },
+      fontFamily: {
+        sans: ['Inter', 'sans-serif'],
+        serif: ['Merriweather', 'serif'],
+      },
+    },
   },
   plugins: [],
 };
