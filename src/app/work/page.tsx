76058ddecb1944d16bc3c5be06448995e23607ac
Mohammad Talim <talimmohammad116@gmail.com>
2023-05-17 22:29:45 +0530

    Make the work page responsive

--- src/app/work/page.tsx
+++ src/app/work/page.tsx
@@ -4,33 +4,35 @@ import Image from 'next/image';
 
 function Work() {
   return (
-    <main className='space-y-4 p-8 lg:p-0'>
-      <h2 className='text-2xl font-bold'>Projects</h2>
-      <section className='mb-8 grid grid-cols-1 gap-8 md:mb-0 md:grid-cols-2'>
-        {projects.map((project, index) => (
-          <article
-            key={index}
-            className='flex flex-col gap-2 rounded-xl bg-dark p-4 shadow-xl'
-          >
-            <Image
-              src={project.src}
-              alt={project.name}
-              className=' rounded-lg'
-            />
-            <h3 className='text-lg font-bold text-lighter'>{project.name}</h3>
-            <p className='flex-1 text-light opacity-80'>
-              {project.description}
-            </p>
-            <div className='mt-4 flex gap-4'>
-              <a href={project.codeLink} target='_blank'>
-                <GithubIcon />
-              </a>
-              <a href={project.liveLink} target='_blank'>
-                <LinkIcon />
-              </a>
-            </div>
-          </article>
-        ))}
+    <main className='mx-auto flex max-w-4xl items-center justify-center p-8 lg:p-0'>
+      <section className='space-y-4'>
+        <h2 className='text-2xl font-bold'>Projects</h2>
+        <section className='mb-8 grid grid-cols-1 gap-8 md:mb-0 md:grid-cols-2'>
+          {projects.map((project, index) => (
+            <article
+              key={index}
+              className='flex flex-col gap-2 rounded-xl bg-dark p-4 shadow-xl'
+            >
+              <Image
+                src={project.src}
+                alt={project.name}
+                className=' rounded-lg'
+              />
+              <h3 className='text-lg font-bold text-lighter'>{project.name}</h3>
+              <p className='flex-1 text-light opacity-80'>
+                {project.description}
+              </p>
+              <div className='mt-4 flex gap-4'>
+                <a href={project.codeLink} target='_blank'>
+                  <GithubIcon />
+                </a>
+                <a href={project.liveLink} target='_blank'>
+                  <LinkIcon />
+                </a>
+              </div>
+            </article>
+          ))}
+        </section>
       </section>
     </main>
   );
