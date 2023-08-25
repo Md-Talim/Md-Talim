4e402b673930b22b15dc244a812d40b7a4e1dce3
Mohammad Talim <talimmohammad116@gmail.com>
2023-08-25 21:53:24 +0530

    Initial commit for new portfolio

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,5 @@
-import Hero from '@/components/Hero';
-
 export default function Home() {
   return (
-    <main className="flex items-center px-8 lg:p-0">
-      <Hero />
-    </main>
-  );
+    <main className="text-5xl">New portfolio</main>
+  )
 }
--- src/assets/index.ts
+++
@@ -1,5 +0,0 @@
-import gameshub from './gameshub.png';
-import logo from './logo.svg';
-import searchx from './searchx.png';
-
-export { gameshub, logo, searchx };
