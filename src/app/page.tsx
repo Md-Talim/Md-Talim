d1c7696871399d863441267bf66900938e429933
Mohammad Talim <talimmohammad116@gmail.com>
2023-08-26 12:34:30 +0530

    Build the Hero Section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,59 @@
-export default function Home() {
+import Container from '@/components/Container';
+import CopyToClipboard from '@/components/CopyToClipboard';
+import {
+  GitHubIcon,
+  LinkedInIcon,
+  TwitterIcon,
+} from '@/components/SocialIcons';
+import Link from 'next/link';
+
+const SocialLink = ({
+  children,
+  href,
+  label,
+}: {
+  children: React.ReactNode;
+  href: string;
+  label: string;
+}) => {
   return (
-    <main className="text-5xl">New portfolio</main>
-  )
-}
+    <Link className="group -m-1 p-1" href={href} aria-label={label}>
+      {children}
+    </Link>
+  );
+};
+
+const Home = () => {
+  return (
+    <>
+      <Container className="mt-9">
+        <div className="max-w-2xl">
+          <h1 className="text-4xl font-bold tracking-tight text-zinc-800 sm:text-5xl">
+            Frontend developer, UI designer, and programmer.
+          </h1>
+          <p className="mt-6 text-base text-zinc-600">
+            I’m Talim, a Frontend developer and user interface designer. I’m
+            final year student of Diploma in Computer Science Engineering.
+          </p>
+          <CopyToClipboard text="mohd.talim.work@gmail.com" />
+          <div className="mt-6 flex gap-4">
+            <SocialLink
+              href="https://github.com/md-talim"
+              label="Follow on Twitter"
+            >
+              <GitHubIcon className="h-8 w-8 fill-zinc-500 transition group-hover:fill-zinc-600" />
+            </SocialLink>
+            <SocialLink
+              href="https://www.linkedin.com/in/md-talim/"
+              label="Follow on Twitter"
+            >
+              <LinkedInIcon className="h-8 w-8 fill-zinc-500 transition group-hover:fill-zinc-600" />
+            </SocialLink>
+          </div>
+        </div>
+      </Container>
+    </>
+  );
+};
+
+export default Home;
---
+++ src/components/Button.tsx
@@ -0,0 +1,30 @@
+import React from 'react';
+import Link from 'next/link';
+import clsx from 'clsx';
+
+interface ButtonProps extends React.ButtonHTMLAttributes<HTMLButtonElement> {
+  variant?: 'primary' | 'secondary';
+}
+
+const variantStyles: Record<string, string> = {
+  primary:
+    'bg-zinc-800 font-semibold text-zinc-100 hover:bg-zinc-700 active:bg-zinc-800 active:text-zinc-100/70',
+  secondary:
+    'bg-zinc-50 font-medium text-zinc-900 hover:bg-zinc-100 active:bg-zinc-100 active:text-zinc-900/60',
+};
+
+const Button: React.FC<ButtonProps> = ({
+  variant = 'primary',
+  className,
+  ...props
+}) => {
+  const combinedClassName = clsx(
+    'inline-flex items-center gap-2 justify-center rounded-md py-2 px-3 text-sm outline-offset-2 transition active:transition-none',
+    variantStyles[variant],
+    className,
+  );
+
+  return <button className={combinedClassName} {...props} />;
+};
+
+export default Button;
