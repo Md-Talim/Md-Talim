59d22ff7d7efc030fbadafeaacd656e0bac403d9
Mohammad Talim <talimmohammad116@gmail.com>
2023-08-28 09:37:35 +0530

    Write copy for contacts section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -41,7 +41,7 @@ const SocialLink = ({
 const Home = () => {
   return (
     <>
-      <Container className="mt-16">
+      <Container className="mt-16" id="about">
         <div className="max-w-3xl">
           <h1 className="text-4xl font-semibold text-zinc-800 sm:text-5xl">
             I Design & Develop Websites.
@@ -86,6 +86,21 @@ const Home = () => {
           </Link>
           <ArrowIcon />
         </Button>
+      </Container>
+
+      <Container className="mt-20">
+        <div className="max-w-3xl">
+          <h2 className="text-3xl font-semibold text-zinc-800 sm:text-4xl">
+            Let&apos;s Connect and Collaborate
+          </h2>
+          <p className="mt-3 text-2xl text-zinc-600">
+            Hey friend 👋,
+            <br />
+            Let&apos;s connect and make something awesome together! If you need
+            help, have questions, or just want to chat, I&apos;m here for you.
+            Feel free to drop me a line anytime!
+          </p>
+        </div>
       </Container>
     </>
   );
