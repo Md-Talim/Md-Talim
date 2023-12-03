727e4b0ac429118bd13b5cad371724128a6a9154
Mohammad Talim <talimmohammad116@gmail.com>
2023-12-03 20:25:15 +0530

    Remove src directory, use app directory

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,6 +1,10 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ['./src/**/*.{ts,tsx}'],
+  content: [
+    './app/**/*.{js,ts,jsx,tsx,mdx}',
+    './pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './components/**/*.{js,ts,jsx,tsx,mdx}',
+  ],
   darkMode: 'class',
   plugins: [require('@tailwindcss/typography')],
   theme: {},
