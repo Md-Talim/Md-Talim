8bea069a6bacd520a9d33e4dc475a7888d440fa3
Mohammad Talim <talimmohammad116@gmail.com>
2023-12-03 20:55:43 +0530

    Update next.js version

--- components/Header.tsx
+++ components/Header.tsx
@@ -130,8 +130,6 @@ const NavItem = ({
 };
 
 const DesktopNavigation = (props: { className: string }) => {
-  const pathname = usePathname();
-
   return (
     <nav {...props}>
       <ul className="flex rounded-full bg-white/90 px-3 text-sm font-medium text-zinc-800 shadow-lg shadow-zinc-800/5 ring-1 ring-zinc-900/5 backdrop-blur dark:bg-zinc-800/90 dark:text-zinc-200 dark:ring-white/10">
