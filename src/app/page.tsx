09927df103414a6a8d8304461cb0df53f8295589
Mohammad Talim <talimmohammad116@gmail.com>
2024-12-13 12:11:16 +0530

    build the hero section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,13 @@
 import { Header } from "@/components/navigation";
+import { HeroSection } from "@/components/sections/hero";
 
 const HomePage = () => {
   return (
     <main>
       <Header />
+      <div className="mx-auto max-w-5xl space-y-32 px-4 pb-24 md:px-8">
+        <HeroSection />
+      </div>
     </main>
   );
 };
---
+++ src/components/utils/reveal.tsx
@@ -0,0 +1,51 @@
+"use client";
+
+import clsx from "clsx";
+import { motion, useAnimation, useInView } from "motion/react";
+import { ReactNode, useEffect, useRef } from "react";
+
+interface Props {
+  children: ReactNode;
+  width?: string;
+}
+
+export const Reveal = ({ children, width = "w-fit" }: Props) => {
+  const ref = useRef(null);
+  const isInView = useInView(ref, { once: true });
+
+  const mainControls = useAnimation();
+  const slideControls = useAnimation();
+
+  useEffect(() => {
+    if (isInView) {
+      mainControls.start("visible");
+      slideControls.start("visible");
+    }
+  }, [isInView]);
+
+  return (
+    <div ref={ref} className={clsx("relative overflow-hidden", width)}>
+      <motion.div
+        variants={{
+          hidden: { opacity: 0, y: 75 },
+          visible: { opacity: 1, y: 0 },
+        }}
+        initial="hidden"
+        animate={mainControls}
+        transition={{ duration: 0.5, delay: 0.25 }}
+      >
+        {children}
+      </motion.div>
+      <motion.div
+        variants={{
+          hidden: { left: 0 },
+          visible: { left: "100%" },
+        }}
+        initial="hidden"
+        animate={slideControls}
+        transition={{ duration: 0.5, ease: "easeIn" }}
+        className="absolute bottom-1 left-0 right-0 top-1 z-20 bg-indigo-500"
+      />
+    </div>
+  );
+};
